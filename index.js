ca795285d0e33aec45255371ee6d308db347dd92
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-24 08:55:25 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(array) {
+  for (let index = 0; index < array.length; index++) {
+    console.log(`Welcome ${array[index]}! You are employee #${index + 1}.`);
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+  let initialRandom = Math.random();
+  while (initialRandom >= 0.5 ) {
+    counter++;
+    initialRandom = Math.random();
+  }
+  return `You got ${counter} tails in a row!`;
+}
